5717c68467249faf9d7b4c33ca57fe81f983d78a
Sameer Kolhar <kolhar730@gmail.com>
2020-12-01 14:35:47 +0530

    improve code slightly 😛

--- 2020/1/main.py
+++ 2020/1/main.py
@@ -1,20 +1,22 @@
 file = open("input", "r")
 fileInput = file.read().split("\n")
-ch = []
 
+ch = []
 for i in fileInput:
   ch.append(int(i))
 
 # part 1
 for i in ch:
-  if i in ch and (2020-i) in ch:
-    print((i, (2020-i)))
+  if (2020-i) in ch:
+    print(i * (2020-i))
+    break
 
 # part 2
 for i in ch:
   for j in ch:
     for k in ch:
       if i + j + k == 2020:
-        print(i,j,k)
+        print(i*j*k)
+        break
 
 # since this is unique, it should be done in a better way
