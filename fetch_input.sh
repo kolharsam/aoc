c9497a646cf041f76ee08320d8d2874be7f163f8
Sameer Kolhar <kolhar730@gmail.com>
2020-12-10 21:11:42 +0530

    add new input fetching script

---
+++ fetch_input.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# helper script to fetch Advent of Code input files
+# this fetches the input and places it in the pwd
+
+current_dir=$PWD
+current_cookie=${AOC_COOKIE}
+
+y_flag=''
+d_flag=''
+
+print_usage() {
+    printf "Usage:\n\t-y YEAR\n\t-d DAY\n\n\tNOTE: Save the \"session\" cookie as AOC_COOKIE in your environment\n"
+}
+
+year=$(date +'%Y')
+day=$(date +'%d')
+
+while getopts 'y:d:' flag;do
+    case "${flag}" in
+        y) y_flag="${OPTARG}" ;;
+        d) d_flag="${OPTARG}" ;;
+        *) print_usage
+           exit 1 ;;
+    esac
+done
+
+# if the flags are empty then, I use today's datea and year
+if [[ -z $y_flag ]]
+then
+    y_flag="$year"
+fi
+
+if [[ -z $d_flag ]]
+then
+    d_flag="$day"
+fi
+
+echo $y_flag
+echo $d_flag
+
+# make the curl request
+curl -XGET -H "Cookie: session=${AOC_COOKIE}" "https://adventofcode.com/${year}/day/${day}/input" -o "${current_dir}/${day}.in"
+
+echo "Done! Fetched day ${day} input."
