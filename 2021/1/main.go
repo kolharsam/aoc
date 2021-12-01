abeda6dbb82307593ecc3f0708b69145546342e2
Sameer Kolhar <kolhar730@gmail.com>
2021-12-01 07:53:10 +0100

    add soln day 1 2021

---
+++ 2021/1/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func readFile(fileName string) []int {
+	body, err := ioutil.ReadFile(fileName)
+	check(err)
+
+	lines := strings.Split(string(body), "\n")
+
+	nums := make([]int, 2000)
+
+	for _, str := range lines {
+		num, err := strconv.Atoi(str)
+		check(err)
+		nums = append(nums, num)
+	}
+
+	return nums
+}
+
+func partOne(nums []int) {
+	cin := 0
+
+	for i, num := range nums {
+		if i != 0 && num > nums[i-1] {
+			cin++
+		}
+	}
+
+	println(cin - 1)
+}
+
+func partTwo(nums []int) {
+	cin := 0
+
+	for i, num := range nums {
+		if i < 3 {
+			continue
+		}
+		prevA := nums[i-3]
+		prevB := nums[i-2]
+		prevC := nums[i-1]
+		prevD := num
+
+		sumPrev := prevA + prevB + prevC
+		currSum := sumPrev - prevA + prevD
+
+		if currSum > sumPrev {
+			cin++
+		}
+
+		if i >= len(nums)-3 {
+			break
+		}
+	}
+
+	println(cin - 1)
+}
+
+func main() {
+	fileName := os.Args[1]
+	nums := readFile(fileName)
+
+	partOne(nums)
+	partTwo(nums)
+}
