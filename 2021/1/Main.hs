f269d8ee18d1df4a5eb4698434a2838aec21053a
Sameer Kolhar <kolhar730@gmail.com>
2021-12-02 15:32:16 +0100

    improve stuff on day 2 2021

--- 2021/1/Main.hs
+++ 2021/1/Main.hs
@@ -3,7 +3,7 @@ import System.Environment (getArgs)
 getData :: String -> IO [Int]
 getData fileName = do
   content <- readFile fileName
-  pure $ map (\x -> read x :: Int) $ lines content
+  pure $ map read $ lines content
 
 solve :: [Bool] -> Int
 solve = length . filter (== True)
@@ -13,8 +13,8 @@ main = do
   args <- getArgs
   nums <- getData (head args)
 
-  print $ solve (prepInput 1 nums)
-  print $ solve (prepInput 3 nums)
+  print . solve $ prepInput 1 nums
+  print . solve $ prepInput 3 nums
   where
     prepInput :: Int -> [Int] -> [Bool]
     prepInput zs nums = zipWith (<) nums (drop zs nums)
--- 2021/2/Main.hs
+++ 2021/2/Main.hs
@@ -1,26 +1,61 @@
-import System.Environment (getArgs)
+{-# LANGUAGE RecordWildCards #-}
 
-getData :: String -> IO [(String, Int)]
+import System.Environment (getArgs)
+-- NOTE: would've been easier with Lenses
+
+type Instruction = (String, Int)
+
+getData :: String -> IO [Instruction]
 getData fileName = do
   content <- readFile fileName
-  pure $ map (\x -> read x :: Int) $ lines content
+  pure $ map (readLine . words) $ lines content
+  where
+    readLine :: [String] -> Instruction
+    readLine [inst, numStr] = (inst, read numStr)
+    readLine _ = error "WTF?"
 
-solve :: [Bool] -> Int
-solve = length . filter (== True)
+solve :: Position -> AimChoice -> [Instruction] -> Position
+solve startPos ac =
+  flip foldl startPos $ \pos (inst, num) -> case inst of
+    "forward" -> moveForward num ac pos
+    "up"      -> updateAimOrDepth num ac pos
+    "down"    -> updateAimOrDepth (-num) ac pos
+    _ -> undefined
+
+makeScore :: Position -> Int
+makeScore ps@Position{..} = _unX * _unZ
 
 data Position = Position {
-    _unX :: Int
-  , _unZ :: Int
+    _unX   :: Int
+  , _unZ   :: Int
   , _unAim :: Int
-}
+} deriving (Show, Eq)
+
+type PositionUpdater = Int -> AimChoice -> Position -> Position
+
+moveForward :: PositionUpdater
+moveForward num ac pos@Position{..} =
+  if ac == WithoutAim
+    then pos { _unX = _unX + num }
+    else pos { _unX = _unX + num, _unZ = _unZ + (_unAim * num) }
+
+updateAimOrDepth :: PositionUpdater
+updateAimOrDepth num ac pos@Position{..} =
+  if ac == WithoutAim
+    then pos { _unZ = _unZ + num }
+    else pos { _unAim = _unAim + num }
+
+data AimChoice = WithAim | WithoutAim
+  deriving(Eq)
 
 main :: IO ()
 main = do
   args <- getArgs
-  nums <- getData (head args)
-
-  print $ solve (prepInput 1 nums)
-  print $ solve (prepInput 3 nums)
+  input <- getData $ head args
+  
+  showScore $ solve' WithoutAim input
+  showScore $ solve' WithAim input
   where
-    prepInput :: Int -> [Int] -> [Bool]
-    prepInput zs nums = zipWith (<) nums (drop zs nums)
+    start = Position 0 0 0
+    solve' = solve start
+    showScore = print . abs . makeScore
