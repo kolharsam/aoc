14abaa01d45afffa985382ddd80c22dc55fccaf8
Sameer Kolhar <kolhar730@gmail.com>
2022-12-06 05:20:25 +0000

    add day 6 soln and prep day 7

--- 2022/6/main.py
+++ 2022/6/main.py
@@ -1,4 +1,14 @@
 import sys
 
 infile = sys.argv[1] if len(sys.argv)>1 else '6.in'
-data = open(infile).read().strip().split('\n')
+sub = int(sys.argv[2]) if len(sys.argv)>2 else 14 # sub = 4 for part 1
+data = open(infile).read().strip().split('\n')[0]
+
+le = len(data)
+
+for i in range(le-sub):
+    seq = data[i:i+sub]
+    sett = set(seq)
+    if sett.__len__() == seq.__len__():
+        print(i+sub)
+        break
