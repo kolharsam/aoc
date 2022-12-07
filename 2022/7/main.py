380828a8e40961280b837c5da095679e67369418
Sameer Kolhar <kolhar730@gmail.com>
2022-12-07 18:05:31 +0000

    add day 7 soln. prep day 8

--- 2022/7/main.py
+++ 2022/7/main.py
@@ -1,4 +1,38 @@
 import sys
+from collections import defaultdict
 
-infile = sys.argv[1] if len(sys.argv)>1 else '6.in'
-data = open(infile).read().strip().split('\n')
+infile = sys.argv[1] if len(sys.argv)>1 else '7.in'
+data = open(infile).read().strip()
+lines = [x for x in data.split('\n')]
+
+SZ = defaultdict(int)
+path = []
+for line in lines:
+    words = line.strip().split()
+    if words[1] == 'cd':
+        if words[2] == '..':
+            path.pop()
+        else:
+            path.append(words[2])
+    elif words[1] == 'ls':
+        continue
+    elif words[0] == 'dir':
+        continue
+    else:
+        sz = int(words[0])
+        for i in range(1, len(path)+1):
+            SZ['/'.join(path[:i])] += sz
+
+max_used = 70000000 - 30000000
+total_used = SZ['/']
+need_to_free = total_used - max_used
+
+p1 = 0
+p2 = 1e9
+for k,v in SZ.items():
+    if v <= 100000:
+        p1 += v
+    if v>=need_to_free:
+        p2 = min(p2, v)
+print(p1)
+print(p2)
